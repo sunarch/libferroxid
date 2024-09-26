c1f8a4284c5c263d646c683a4191a3dfbe15cd46
András Németh <sunarch@protonmail.com>
2024-09-26 07:16:16 +0200

    Simplify conversion by removing dependency and extract components

--- src/conversion/endian.rs
+++ src/conversion/endian.rs
@@ -2,13 +2,26 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// dependencies
-use byteorder::{ByteOrder, LittleEndian};
+pub fn u16_low_byte(value: u16) -> u8 {
+    value as u8
+}
 
+pub fn u16_high_byte(value: u16) -> u8 {
+    (value >> 8) as u8
+}
 
-pub fn u16_as_little_endian_array(value: u16) -> [u8; 2] {
-    let mut buffer: [u8; 2] = [0; 2];
-    LittleEndian::write_u16(&mut buffer, value);
+pub fn u16_to_little_endian_array(value: u16) -> [u8; 2] {
+    [u16_low_byte(value), u16_high_byte(value)]
+}
 
-    return buffer;
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn little_endian_is_correct() {
+        let value: u16 = 0x1234;
+        let result: [u8; 2] = u16_to_little_endian_array(value);
+        assert_eq!(result, [0x34, 0x12]);
+    }
 }
